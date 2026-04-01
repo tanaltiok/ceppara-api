b689ce3a3003aa6c25bfb5fbd3b19d3a91dd4b3d
tanaltiok <tanaltiok@outlook.com>
2026-04-01 00:49:14 -0700

    Update rates.js

--- api/rates.js
+++ api/rates.js
@@ -2,21 +2,20 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   try {
     const [goldRes, fxRes] = await Promise.all([
-      fetch('https://www.doviz.com/api/v1/currencies/all/latest'),
+      fetch('https://www.doviz.com/api/v1/currencies/gram-altin/latest'),
       fetch('https://open.er-api.com/v6/latest/USD')
     ]);
 
     const goldData = await goldRes.json();
     const fxData = await fxRes.json();
 
-    const gramGold = goldData.find(i => i.code === 'gram-altin');
     const usdTry = fxData.rates.TRY;
     const eurTry = usdTry / fxData.rates.EUR;
     const gbpTry = usdTry / fxData.rates.GBP;
     const cadTry = usdTry / fxData.rates.CAD;
 
     res.json({
-      gramGold: gramGold?.selling,
+      gramGold: goldData?.selling,
       usdTry,
       eurTry,
       gbpTry,
